c66f68e583f6422179c060c19a70564014eab726
Eugene Prilepin <prilepin@smedx.com>
2017-06-18 14:26:46 +0300

    Fix declaring

--- gmm/itkICPPointSetToPointSetMetric.hxx
+++ gmm/itkICPPointSetToPointSetMetric.hxx
@@ -52,23 +52,23 @@ void ICPPointSetToPointSetMetric<TFixedPointSet, TMovingPointSet>::GetDerivative
 template <typename TFixedPointSet, typename TMovingPointSet>
 void ICPPointSetToPointSetMetric<TFixedPointSet, TMovingPointSet>::GetValueAndDerivative(const TransformParametersType & parameters, MeasureType & value, DerivativeType  & derivative) const
 {
-  m_Transform->SetParameters(parameters);
+  this->m_Transform->SetParameters(parameters);
 
-  if (derivative.size() != m_NumberOfParameters) {
-    derivative.set_size(m_NumberOfParameters);
+  if (derivative.size() != this->m_NumberOfParameters) {
+    derivative.set_size(this->m_NumberOfParameters);
   }
 
   derivative.Fill(NumericTraits<typename DerivativeType::ValueType>::ZeroValue());
   value = NumericTraits<MeasureType>::ZeroValue();
   GradientType gradient;
 
-  for (MovingPointIterator movingIter = m_MovingPointSet->GetPoints()->Begin(); movingIter != m_MovingPointSet->GetPoints()->End(); ++movingIter) {
+  for (MovingPointIterator movingIter = this->m_MovingPointSet->GetPoints()->Begin(); movingIter != this->m_MovingPointSet->GetPoints()->End(); ++movingIter) {
     const typename MovingPointSetType::PointType movingPoint = movingIter.Value();
-    const typename MovingPointSetType::PointType transformedPoint = m_Transform->TransformPoint(movingPoint);
+    const typename MovingPointSetType::PointType transformedPoint = this->m_Transform->TransformPoint(movingPoint);
 
     // find closest point
     size_t idx = m_FixedPointsLocator->FindClosestPoint(transformedPoint);
-    const typename FixedPointSetType::PointType fixedPoint = m_FixedPointSet->GetPoint(idx);
+    const typename FixedPointSetType::PointType fixedPoint = this->m_FixedPointSet->GetPoint(idx);
 
     // compute value
     value += transformedPoint.SquaredEuclideanDistanceTo(fixedPoint);
@@ -79,19 +79,19 @@ void ICPPointSetToPointSetMetric<TFixedPointSet, TMovingPointSet>::GetValueAndDe
     }
 
     // compute derivatives
-    m_Transform->ComputeJacobianWithRespectToParametersCachedTemporaries(movingPoint, m_Jacobian, m_JacobianCache);
+    this->m_Transform->ComputeJacobianWithRespectToParametersCachedTemporaries(movingPoint, this->m_Jacobian, this->m_JacobianCache);
 
-    for (size_t par = 0; par < m_NumberOfParameters; ++par) {
+    for (size_t par = 0; par < this->m_NumberOfParameters; ++par) {
       for (size_t dim = 0; dim < PointDimension; ++dim) {
-        derivative[par] += m_Jacobian(dim, par) * gradient[dim];
+        derivative[par] += this->m_Jacobian(dim, par) * gradient[dim];
       }
     }
   }
 
-  value /= m_MovingPointSet->GetNumberOfPoints();
+  value /= this->m_MovingPointSet->GetNumberOfPoints();
 
-  for (size_t par = 0; par < m_NumberOfParameters; ++par) {
-    derivative[par] /= m_MovingPointSet->GetNumberOfPoints();
+  for (size_t par = 0; par < this->m_NumberOfParameters; ++par) {
+    derivative[par] /= this->m_MovingPointSet->GetNumberOfPoints();
   }
 }
 }
