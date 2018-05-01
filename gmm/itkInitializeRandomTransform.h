08428ff0e402410fa986882844af14267db40dd5
Ruslan N. Kosarev <ruslan.kosarev@gmail.com>
2018-05-02 01:06:51 +0300

    class to initialize random transform implemented

---
+++ gmm/itkInitializeRandomTransform.h
@@ -0,0 +1,291 @@
+#pragma once
+
+#include <itkObject.h>
+#include <itkTranslationTransform.h>
+#include <itkVersorRigid3DTransform.h>
+#include <itkSimilarity3DTransform.h>
+#include <itkScaleSkewVersor3DTransform.h>
+#include <itkMersenneTwisterRandomVariateGenerator.h>
+
+namespace itk
+{
+  template<typename TParametersValueType>
+  class InitializeRandomTransform : public Object
+  {
+  public:
+    /** Standard class typedefs. */
+    typedef InitializeRandomTransform  Self;
+    typedef Object                     Superclass;
+    typedef SmartPointer<Self>         Pointer;
+    typedef SmartPointer<const Self>   ConstPointer;
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+    itkTypeMacro(InitializeRandomTransform, Object);
+
+    enum Transform
+    {
+      Translation = 0,
+      Versor3D = 1,
+      Similarity = 2,
+      ScaleSkewVersor3D = 3
+    };
+
+    /** typedefs */
+    itkStaticConstMacro(PointDimension, unsigned int, 3U);
+    static_assert(PointDimension == 3U, "Invalid dimension. Dimension 3 is supported.");
+
+    typedef typename itk::Transform<TParametersValueType, PointDimension>  TransformType;
+    typedef typename TransformType::InputPointType                         InputPointType;
+    typedef typename TransformType::OutputPointType                        OutputPointType;
+    typedef typename TransformType::OutputVectorType                       OutputVectorType;
+
+    typedef typename TransformType::ParametersType                         ParametersType;
+
+    /** Type of the transform generator. */
+    typedef itk::Statistics::MersenneTwisterRandomVariateGenerator TransformGeneratorType;
+
+    // Get transform
+    itkGetObjectMacro(Transform, TransformType);
+
+    // Set/Get type of transform
+    itkSetEnumMacro(TypeOfTransform, Transform);
+    itkGetEnumMacro(TypeOfTransform, Transform);
+    void SetTypeOfTransform(const size_t & type) { this->SetTypeOfTransform(static_cast<Transform>(type)); }
+
+    // Set/Get the center 
+    itkSetMacro(Center, InputPointType);
+    itkGetMacro(Center, InputPointType);
+
+    // Set/Get the bounds
+    itkSetMacro(TranslationBounds, double);
+    itkGetMacro(TranslationBounds, double);
+
+    itkSetMacro(RotationBounds, double);
+    itkGetMacro(RotationBounds, double);
+
+    itkSetMacro(ScalingBounds, double);
+    itkGetMacro(ScalingBounds, double);
+
+    itkSetMacro(SkewBounds, double);
+    itkGetMacro(SkewBounds, double);
+
+    void Initialize()
+    {
+      switch (m_TypeOfTransform) {
+      case Transform::Translation: {
+        // Translation transform
+        typedef typename itk::TranslationTransform<TParametersValueType, PointDimension> TranslationTransformType;
+        typename TranslationTransformType::Pointer transform = TranslationTransformType::New();
+
+        m_NumberOfTranslationComponents = 3;
+
+        m_Transform = transform;
+        ParametersType parameters = m_Transform->GetParameters();
+
+        for (size_t n = 0; n < m_NumberOfTranslationComponents; ++n)
+        {
+          parameters[n] = m_TransformGenerator->GetUniformVariate(-m_TranslationBounds, m_TranslationBounds);
+        }
+
+        m_Transform->SetParameters(parameters);
+        break;
+      }
+      case Transform::Versor3D: {
+        // VersorRigid3DTransform
+        typedef itk::VersorRigid3DTransform<TParametersValueType> VersorRigid3DTransformType;
+        typename VersorRigid3DTransformType::Pointer transform = VersorRigid3DTransformType::New();
+        transform->SetIdentity();
+        transform->SetCenter(m_Center);
+
+        m_NumberOfRotationComponents = 3;
+        m_NumberOfTranslationComponents = 3;
+
+        m_Transform = transform;
+        ParametersType parameters = m_Transform->GetParameters();
+
+        int count = 0;
+
+        // rotation
+        double prod = itk::NumericTraits<double>::epsilon();
+        double angle = m_TransformGenerator->GetUniformVariate(-m_RotationBounds, m_RotationBounds);
+
+        for (size_t n = 0; n < m_NumberOfRotationComponents; ++n, ++count) 
+        {
+          parameters[n] = m_TransformGenerator->GetUniformVariate(-1, 1);
+          prod += parameters[n] * parameters[n];
+        }
+
+        for (size_t n = 0; n < m_NumberOfRotationComponents; ++n) 
+        {
+          parameters[n] *= angle / sqrt(prod);
+        }
+
+        // translation
+        for (size_t n = 0; n < m_NumberOfTranslationComponents; ++n, ++count)
+        {
+          parameters[count] = m_TransformGenerator->GetUniformVariate(-m_TranslationBounds, m_TranslationBounds);
+        }
+
+        m_Transform->SetParameters(parameters);
+        break;
+      }
+
+      case Transform::Similarity:{
+        // Similarity3DTransform
+        typedef itk::Similarity3DTransform<TParametersValueType> Similarity3DTransformType;
+        typename Similarity3DTransformType::Pointer transform = Similarity3DTransformType::New();
+        transform->SetIdentity();
+        transform->SetCenter(m_Center);
+
+        m_NumberOfRotationComponents = 3;
+        m_NumberOfTranslationComponents = 3;
+        m_NumberOfScalingComponents = 1;
+
+        m_Transform = transform;
+        ParametersType parameters = m_Transform->GetParameters();
+
+        int count = 0;
+
+        // rotation
+        double prod = itk::NumericTraits<double>::epsilon();
+        double angle = m_TransformGenerator->GetUniformVariate(-m_RotationBounds, m_RotationBounds);
+
+        for (size_t n = 0; n < m_NumberOfRotationComponents; ++n, ++count) 
+        {
+          parameters[n] = m_TransformGenerator->GetUniformVariate(-1, 1);
+          prod += parameters[n] * parameters[n];
+        }
+
+        for (size_t n = 0; n < m_NumberOfRotationComponents; ++n) 
+        {
+          parameters[n] *= angle / sqrt(prod);
+        }
+
+        // translation
+        for (size_t n = 0; n < m_NumberOfTranslationComponents; ++n, ++count) 
+        {
+          parameters[count] = m_TransformGenerator->GetUniformVariate(-m_TranslationBounds, m_TranslationBounds);
+        }
+
+        // scaling
+        for (size_t n = 0; n < m_NumberOfScalingComponents; ++n, ++count)
+        {
+          parameters[count] = m_TransformGenerator->GetUniformVariate(1 - m_ScalingBounds, 1 + m_ScalingBounds);
+        }
+
+        m_Transform->SetParameters(parameters);
+        break;
+      }
+
+      case Transform::ScaleSkewVersor3D:{
+        typedef itk::ScaleSkewVersor3DTransform<TParametersValueType> ScaleSkewVersor3DTransformType;
+        typename ScaleSkewVersor3DTransformType::Pointer transform = ScaleSkewVersor3DTransformType::New();
+        transform->SetIdentity();
+        transform->SetCenter(m_Center);
+
+        m_NumberOfRotationComponents = 3;
+        m_NumberOfTranslationComponents = 3;
+        m_NumberOfScalingComponents = 3;
+        m_NumberOfSkewComponents = 6;
+
+        m_Transform = transform;
+        ParametersType parameters = m_Transform->GetParameters();
+
+        int count = 0;
+
+        // rotation
+        double prod = itk::NumericTraits<double>::epsilon();
+
+        for (size_t n = 0; n < m_NumberOfRotationComponents; ++n, ++count) 
+        {
+          parameters[n] = m_TransformGenerator->GetUniformVariate(-1, 1);
+          prod += parameters[n] * parameters[n];
+        }
+
+        double angle = m_TransformGenerator->GetUniformVariate(-m_RotationBounds, m_RotationBounds);
+
+        for (size_t n = 0; n < m_NumberOfRotationComponents; ++n, ++count) 
+        {
+          parameters[n] *= angle / sqrt(prod);
+        }
+
+        // translation
+        for (size_t n = 0; n < m_NumberOfTranslationComponents; ++n, ++count) 
+        {
+          parameters[n] = m_TransformGenerator->GetUniformVariate(-m_TranslationBounds, m_TranslationBounds);
+        }
+
+        // scaling
+        for (size_t n = 0; n < m_NumberOfScalingComponents; ++n, ++count) 
+        {
+          parameters[n] = m_TransformGenerator->GetUniformVariate(1 - m_ScalingBounds, 1 + m_ScalingBounds);
+        }
+
+        // skew
+        for (size_t n = 0; n < m_NumberOfSkewComponents; ++n, ++count) 
+        {
+          parameters[n] = m_TransformGenerator->GetUniformVariate(-m_SkewBounds, m_SkewBounds);
+        }
+
+        m_Transform->SetParameters(parameters);
+        break;
+      }
+      default:
+        itkExceptionMacro(<< "Invalid type of the input transform");
+      }
+
+    }
+
+  protected:
+    InitializeRandomTransform() 
+    {
+      m_TransformGenerator = TransformGeneratorType::New();
+      m_TransformGenerator->Initialize(m_RandomSeed);
+    }
+    virtual ~InitializeRandomTransform() {};
+
+    virtual void PrintSelf(std::ostream & os, itk::Indent indent) const ITK_OVERRIDE
+    {
+      Superclass::PrintSelf(os, indent);
+      os << std::endl;
+
+      os << "Center " << m_Center << std::endl;
+      os << std::endl;
+
+      os << "Transform " << m_Transform->GetTransformTypeAsString() << std::endl;
+      os << indent << "Center           " << m_Center << std::endl;
+      os << indent << "Fixed parameters " << m_Transform->GetFixedParameters() << ", " << m_Transform->GetNumberOfFixedParameters() << std::endl;
+      os << indent << "Parameters       " << m_Transform->GetParameters() << ", " << m_Transform->GetNumberOfParameters() << std::endl;
+      os << std::endl;
+
+      os << "Bounds" << std::endl;
+      os << indent << "Translation bounds " << m_TranslationBounds << std::endl;
+      os << indent << "Rotation bounds    " << m_RotationBounds << std::endl;
+      os << indent << "Scaling bounds     " << m_ScalingBounds << std::endl;
+      os << indent << "Skew bounds        " << m_SkewBounds << std::endl;
+    }
+
+    Transform m_TypeOfTransform = Transform::Similarity;
+    typename TransformType::Pointer m_Transform = nullptr;
+
+    InputPointType m_Center;
+
+    typename TransformGeneratorType::Pointer m_TransformGenerator;
+    int m_RandomSeed = 0;
+
+    double m_TranslationBounds = 100;
+    double m_RotationBounds = itk::Math::pi_over_4;
+    double m_ScalingBounds = 0.1;
+    double m_SkewBounds = 0.1;
+
+    size_t m_NumberOfTranslationComponents = 0;
+    size_t m_NumberOfRotationComponents = 0;
+    size_t m_NumberOfScalingComponents = 0;
+    size_t m_NumberOfSkewComponents = 0;
+
+  private:
+    InitializeRandomTransform(const Self &) ITK_DELETE_FUNCTION;
+    void operator=(const Self &) ITK_DELETE_FUNCTION;
+  };
+}
